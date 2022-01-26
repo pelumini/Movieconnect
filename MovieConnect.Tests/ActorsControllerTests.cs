3f225680e9f775afb238d3b9439c8ac63ca306e2
pelumini <pelumini@gmail.com>
2022-01-26 19:34:13 +0000

    more tests added

--- MovieConnect.Tests/ActorsControllerTests.cs
+++ MovieConnect.Tests/ActorsControllerTests.cs
@@ -43,6 +43,8 @@ namespace MovieConnect.Tests
 
             var viewResult = Assert.IsType<ViewResult>(result);
             var actors = Assert.IsType<List<Actor>>(viewResult.Model);
+
+            // Assert
             Assert.Equal(2, actors.Count);
         }
 
@@ -54,6 +56,58 @@ namespace MovieConnect.Tests
 
             // Assert
             Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public async Task Create_Action_InvalidModelState_ReturnsView()
+        {
+            _sut.ModelState.AddModelError("Bio", "Biography is required");
+
+            var actor = new Actor { Id=10, FullName= "Timothée Chalamet", ProfilePictureURL=""};
+
+            var result = await _sut.Create(actor);
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var testActor = Assert.IsType<Actor>(viewResult.Model);
+
+            // Assert
+            Assert.Equal(actor.FullName, testActor.FullName);
+            Assert.Equal(actor.ProfilePictureURL, testActor.ProfilePictureURL);
+        }
+
+        [Fact]
+        public async Task Create_Action_InvalidModelState_CreateNeverExecutes()
+        {
+            _sut.ModelState.AddModelError("Bio", "Biography is required");
+
+            var actor = new Actor { Id = 10, FullName = "Timothée Chalamet", ProfilePictureURL = "" };
+
+            await _sut.Create(actor);
+
+            _mockService.Verify(x => x.AddAsync(It.IsAny<Actor>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task Create_Action_ModelStateValid_CreateActorCalledOnce()
+        {
+            Actor? act = null;
+            _mockService.Setup(r => r.AddAsync(It.IsAny<Actor>()))
+                .Callback<Actor>(x => act = x);
+
+            var actor = new Actor
+            {
+                FullName = "Timothée Chalamet",
+                ProfilePictureURL = "https://dotnethow.net/images/actors/actor-2.jpeg",
+                Bio = "Vibrant Young Actor"
+            };
+
+            await _sut.Create(actor);
+
+            _mockService.Verify(x => x.AddAsync(It.IsAny<Actor>()), Times.Once);
+
+            Assert.Equal(act.FullName, actor.FullName);
+            Assert.Equal(act.ProfilePictureURL, actor.ProfilePictureURL);
+            Assert.Equal(act.Bio, actor.Bio);
         }
 
 
